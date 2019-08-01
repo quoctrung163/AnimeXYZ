1e7e160417e24f0d3fb5b81b5f87c852a96e32db
miyucoder <miyucoder@gmail.com>
2019-08-01 08:34:52 +0700

    done basic pagination

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -6,8 +6,80 @@ module.exports.index = function (req, res, next) {
 
     let start = (page - 1) * perPage;
     let end = page * perPage;
+
+    let itemsAll = db.get('items').value();
+    let items = itemsAll.slice(start, end);
+
+    let countAllPages = Math.ceil(itemsAll.length / perPage);
+    let allPagesShow = [];
+    let dotBefore = false;
+    let dotAfter = false;
+    let pageNext = 0;
+    let pagePrev = 0;
+
+    if (page === 1) {
+        pagePrev = 1;
+        pageNext = 2;
+    }
+
+    if (page === countAllPages) {
+        pagePrev = countAllPages - 1;
+        pageNext = countAllPages;
+    }
+
+    if (page > 1 && page < countAllPages) {
+        pageNext = page + 1;
+        pagePrev = page - 1;
+    }
+
+    if (page + 3 <= countAllPages) {
+        dotAfter = true;
+    }
+
+    if (page === countAllPages - 2) {
+        dotAfter = true;
+    }
+
+    if (page >= 3) {
+        dotBefore = true;
+    }
+
+    if (page < countAllPages && page > 1) {
+        allPagesShow = [page - 1, page, page + 1];
+    }
+
+    if (countAllPages === 2) {
+        allPagesShow = [1, 2];
+    }
+
+    if (countAllPages === 3) {
+        allPagesShow = [1, 2, 3];
+    }
+
+    if (page === 1 && page < countAllPages) {
+
+        if (countAllPages >= 3) {
+            allPagesShow = [1, 2, 3];
+        }
+    }
+
+    if (page === countAllPages - 1 && countAllPages > 3) {
+        allPagesShow = [page - 1, page, page + 1]
+    }
+
+    if (page === countAllPages && countAllPages > 3) {
+        allPagesShow = [page - 2, page - 1, page]
+    }
+
     res.render('index', {
-        items: db.get('items').value().slice(start, end)
+        items: items,
+        pageNow: page,
+        pagePrev: pagePrev,
+        pageNext: pageNext,
+        endPage: countAllPages,
+        allPagesShow: allPagesShow,
+        dotAfter: dotAfter,
+        dotBefore: dotBefore
     });
     next();
 };
